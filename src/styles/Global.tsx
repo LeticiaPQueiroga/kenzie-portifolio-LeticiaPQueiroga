da9c1739b6aab4854563217d6827f2782647310a
Leticia <leticiaqueiroga25@gmail.com>
2023-08-10 10:17:32 -0300

    feat: create style and edit data

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -81,11 +81,10 @@ export const globalStyles = globalCss({
 });
 
 export const GlobalStyle = ({ children }: GlobalStyleProps): JSX.Element => {
-
   const theme = createTheme({
     colors: {
-      brand1: "#623CEA",
-      brand2: "#311E75",
+      brand1: "#972ADE",
+      brand2: "#bca0f4",
 
       grey0: "#0A0A0B",
       grey1: "#121214",
--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Leticia Queiroga",
+  githubUser: "LeticiaPQueiroga",
+  linkedinUser: "leticiapqueiroga",
+  whatsappNumber: "+5533991403959",
+  emailUser: "leticiaqueiroga25@gmail.com",
+  instagramUser: "queirogaleticia",
   facebookUser: "",
 };
